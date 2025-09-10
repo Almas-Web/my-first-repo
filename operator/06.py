95496730fa532c1d54e7a663e4f83c3caa892c4c
Almas-Web <almashossen028@gmail.com>
2025-09-10 21:24:19 +0600

    Added operator/06.py with new python example

---
+++ operator/06.py
@@ -0,0 +1,19 @@
+# membership operator 
+club_member=[
+    "Almas",
+    "Sakib",
+    "Tamim",
+    "Nipun",
+    "Bakkar"
+]
+
+new_person = "Rubel"
+dcision =new_person in club_member
+print(dcision)
+dcision= new_person not in club_member
+print(dcision)
+
+#direct use 
+print("Almas" in club_member)
+print("Sakib" in club_member)
+print("tamim" in club_member) # case sensitive
